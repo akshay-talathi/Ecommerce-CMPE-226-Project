98c2e023c7e61ef9aef06aaeae252dd50649c70a
Akshay Talathi <akshay.talathi@sjsu.edu>
2016-03-12 14:07:10 -0800

    Update customers.js

--- routes/customers.js
+++ routes/customers.js
@@ -1,42 +1,42 @@
-							var mysqldb = require('../mysqldb.js');
-							var util = require('util');
+	var mysqldb = require('../mysqldb.js');
+	var util = require('util');
 
 
-							exports.login = function(req, res) {
-							    // console.log(mysqldb.getName());
+	exports.login = function(req, res) {
+	    // console.log(mysqldb.getName());
 
 
-							    res.render('login', {
-							        page_title: "Login"
-							    });
-							};
+	    res.render('login', {
+	        page_title: "Login"
+	    });
+	};
 
-							exports.home = function(req, res) {
-							    var input = JSON.parse(JSON.stringify(req.body));
-							    var data = {
-							        email: input.email,
-							        password: input.password,
-							    };
+	exports.home = function(req, res) {
+	    var input = JSON.parse(JSON.stringify(req.body));
+	    var data = {
+	        email: input.email,
+	        password: input.password,
+	    };
 
 
-							    if (req.session.firstname === undefined) {
-							        res.redirect("/");
-							    } else {
+	    if (req.session.firstname === undefined) {
+	        res.redirect("/");
+	    } else {
 
-							        var connection = mysqldb.getConnection();
-							        connection.connect();
-							        console.log("----------------data    " + data);
-							        var query = connection.query("SELECT * from ecommerce1.User WHERE email = ? ", [data.email], function(err, rows) {
-							            if (err)
-							                console.log("Error fecthing details : %s", err);
-							            if (rows[0].password == data.password) {
-							                console.log(rows);
+	        var connection = mysqldb.getConnection();
+	        connection.connect();
+	        console.log("----------------data    " + data);
+	        var query = connection.query("SELECT * from ecommerce1.User WHERE email = ? ", [data.email], function(err, rows) {
+	            if (err)
+	                console.log("Error fecthing details : %s", err);
+	            if (rows[0].password == data.password) {
+	                console.log(rows);
 
-							            } else {
-							                res.redirect('/');
-							            }
-							            console.log();
-							        });
-							        connection.end();
-							    }
-							};
+	            } else {
+	                res.redirect('/');
+	            }
+	            console.log();
+	        });
+	        connection.end();
+	    }
+	};
